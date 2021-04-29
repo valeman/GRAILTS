e4d267c1bc534718f278d3ef960b5d8aeab55b83
karhankaan <56487988+karhankaan@users.noreply.github.com>
2021-04-29 04:46:51 -0500

    scale grail

--- Causal_Test.py
+++ Causal_Test.py
@@ -6,8 +6,52 @@ from Causal_inference import generate_synthetic, granger_matrix, check_with_orig
 import csv
 
 
-def test(n = 100, lag = 2, m = 128):
+
+def test_only_grail(n = 100, lag = 2, m = 128, neighbor_param = [10,100]):
     #try not setting gamma
+    d = int(min(max(np.ceil(0.4 * 2*n), 20), 100))
+    representation = Representation.GRAIL(kernel="SINK", d = 100, gamma = 1)
+
+    TS, trueMat = generate_synthetic(n, m = m, lag = lag, ar = [1, 0.5]) #try changing ar
+
+    grailMat = np.zeros((n, n))
+
+    result_by_neighbor = {}
+
+    TRAIN_TS, TEST_TS = representation.get_rep_train_test(TS, TS)
+
+    for neighbor_num in neighbor_param:
+        if neighbor_num >= n:
+            continue
+        np.random.seed(0)
+        neighbors, _, _ = kNN(TRAIN_TS, TEST_TS, method="ED", k=neighbor_num, representation=None, use_exact_rep=True,
+                              pq_method="opq")
+
+        t = time()
+        for i in range(n):
+           for j in neighbors[i]:
+               if j != i:
+                  grailMat[i,j] = granger_causality(TS[j], TS[i], lag)
+        prunedtime = time() - t
+
+        grail_results = check_with_original(trueMat, grailMat)
+        result_by_neighbor[neighbor_num] = {'precision' : grail_results[0], 'recall' : grail_results[1],
+                                            'fscore' : grail_results[2], 'runtime' : prunedtime}
+
+    return result_by_neighbor
+
+
+def test(n = 100, lag = 2, m = 128):
+    """
+    Perform tests of accuracy and time on GRAIL and standard granger causality tests.
+    :param n: Number of time series
+    :param lag: The lag of caulality
+    :param m: time series size
+    :return: brute_results is the results of the standard method,
+    results_by_neighbor is the results with GRAiL Pruning
+    """
+    #try not setting gamma
+    d = int(min(max(np.ceil(0.4 * 2*n), 20), 100))
     representation = Representation.GRAIL(kernel="SINK", d = 50, gamma = 1)
 
     TS, trueMat = generate_synthetic(n, m = m, lag = lag, ar = [1, 0.5]) #try changing ar
@@ -66,6 +110,10 @@ def test(n = 100, lag = 2, m = 128):
 
 
 def compare_with_standard():
+    """
+    Compare Grail pruning with standard granger causality method
+    :return:
+    """
     csvfile = open('causal_inference.csv', 'w')
     csvwriter = csv.writer(csvfile)
     m =128
@@ -76,10 +124,33 @@ def compare_with_standard():
             csvwriter.writerow([n] + [lag] + ['brute'] + list(brute_results.values()))
             for n_num in result_by_neighbor:
                 csvwriter.writerow([n] + [lag] + [n_num] + list(result_by_neighbor[n_num].values()))
+
+            csvfile.flush()
+    csvfile.close()
+
+def scale_grail():
+    """
+    Show that granger causality with GRAIL can scale
+    :return:
+    """
+    csvfile = open('scale_grail.csv', 'w')
+    csvwriter = csv.writer(csvfile)
+    m =128
+    lags = [2,5,10]
+
+    for n in range(100, 10000000, 500):
+        for lag in lags:
+            print(n, lag)
+            result_by_neighbor= test_only_grail(n,lag,m)
+            for n_num in result_by_neighbor:
+                csvwriter.writerow([n] + [lag] + [n_num] + list(result_by_neighbor[n_num].values()))
+
+            csvfile.flush()
     csvfile.close()
 
 
-compare_with_standard()
+if __name__ == '__main__':
+    scale_grail()
 
 
 
